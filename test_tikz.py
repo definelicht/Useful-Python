cd9b1806b4722d687d5af31e7651196723074e0f
Johannes de Fine Licht <johannes@musicmedia.dk>
2016-07-18 14:26:42 +0200

    Compile and show standalone tikz diagrams

---
+++ test_tikz.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import os, re, subprocess, sys
+
+def test_tikz(path, viewer="evince", buildFolder="/tmp/"):
+
+  path = sys.argv[1]
+  filename = os.path.basename(path)
+
+  m = re.search("(.+).tex", filename)
+  if not m:
+    print("Input file must be a .tex file.")
+    sys.exit(1)
+  stripped = m.group(1)
+
+  outDir = os.path.join(buildFolder, filename)
+
+  try:
+    os.makedirs(outDir)
+  except FileExistsError:
+    pass
+
+  with open(os.path.join(outDir, filename), "w") as outFile:
+    with open(path, "r") as inFile:
+      outFile.write("\\documentclass[crop,tikz]{standalone}\n" +
+                    "\\begin{document}\n" +
+                    inFile.read() +
+                    "\\end{document}")
+
+  proc = subprocess.Popen(["pdflatex", filename], cwd=outDir)
+  proc.communicate()
+  if proc.returncode != 0:
+    print(err)
+    sys.exit(1)
+
+  subprocess.call([viewer, os.path.join(outDir, stripped + ".pdf")])
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: <path to .tex file>")
+    sys.exit(1)
+  test_tikz(sys.argv[1])
