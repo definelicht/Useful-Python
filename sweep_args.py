d207c8253e566d8337598050cfbbcc9961149e0e
Johannes de Fine Licht <johannes@musicmedia.dk>
2016-10-28 17:19:46 +0200

    Factored function out of main script

--- sweep_args.py
+++ sweep_args.py
@@ -6,11 +6,14 @@ def run_process(command):
   p.wait()
   return p.returncode
 
+def sweep_args(program, args):
+  for perm in itertools.product(*args):
+    if run_process([sys.argv[1]] + list(perm)) != 0:
+      raise RuntimeError("Execution failed with arguments:", perm)
+
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     print("Usage: <command> [<inputs to arg separated by commas>...]")
     sys.exit(1)
   args = [arg.split(",") for arg in sys.argv[2:]]
-  for perm in itertools.product(*args):
-    if run_process([sys.argv[1]] + list(perm)) != 0:
-      print("Execution failed with arguments:", perm)
+  sweep_args(sys.argv[1], args)
