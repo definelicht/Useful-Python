9f7229debe591fe81931dfe2153fdbd521f6125f
Johannes de Fine Licht <johannes@musicmedia.dk>
2016-11-14 13:42:24 +0100

    Use finditer and allow words with apostrophes

--- countwords.py
+++ countwords.py
@@ -2,9 +2,10 @@
 import re, sys
 
 def count_words(fullString):
-  fullString = re.sub("[^a-zA-z ]", "", fullString) # Remove non-characters
   counts = {} # Create an empty dictionary (works as a hash table)
-  for word in fullString.split(" "): # Divide string into individual words
+  # Divide string into individual words
+  for word in re.finditer("[A-Z][A-Z]+|[a-zA-Z][a-z']*", fullString):
+    word = word.group(0).lower()
     if word not in counts:
       counts[word] = 1 # If word doesn't exist yet, create a new entry
     else:
